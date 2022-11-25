378342d2bb45b49d1f9419e0488141d9c3b4da31
AmalAlby <amalalbyn@gmail.com>
2022-11-25 17:11:52 +0530

    Course add created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddcourseComponent } from './addcourse/addcourse.component';
+import { RouterModule, Routes } from '@angular/router';
+import { compileClassMetadata } from '@angular/compiler';
+
+const myroute:Routes=[
+
+  {
+    path:"",
+    component:AddcourseComponent
+  }
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddcourseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
